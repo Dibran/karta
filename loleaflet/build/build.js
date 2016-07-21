152faaf90e9103d7c2a6e490024e7f5bc86a529a
Pranav Kant <pranavk@collabora.co.uk>
2016-07-21 23:37:59 +0530

    loleaflet: Cherry-pick source map build changes from upstream
    Contributed by Iván Sánchez Ortega to upstream Leaflet
    repository.

--- loleaflet/build/build.js
+++ loleaflet/build/build.js
@@ -1,6 +1,7 @@
 var fs = require('fs'),
     UglifyJS = require('uglify-js'),
     zlib = require('zlib'),
+    SourceNode = require('source-map').SourceNode,
 
     deps = require('./deps.js').deps;
     adminDeps = require('./adminDeps.js').adminDeps;
@@ -82,6 +83,46 @@ function loadSilently(path) {
 	}
 }
 
+// Concatenate the files while building up a sourcemap for the concatenation,
+// and replace the line defining L.version with the string prepared in the jakefile
+function bundleFiles(files, copy, version) {
+	var node = new SourceNode(null, null, null, '');
+
+	node.add(new SourceNode(null, null, null, copy + '(function (window, document, undefined) {'));
+
+	for (var i = 0, len = files.length; i < len; i++) {
+		var contents = fs.readFileSync(files[i], 'utf8');
+
+		if (files[i] === 'src/Leaflet.js') {
+			contents = contents.replace(
+				new RegExp('version: \'.*\''),
+				'version: ' + JSON.stringify(version)
+			);
+		}
+
+		var lines = contents.split('\n');
+		var lineCount = lines.length;
+		var fileNode = new SourceNode(null, null, null, '');
+
+		fileNode.setSourceContent(files[i], contents);
+
+		for (var j=0; j<lineCount; j++) {
+			fileNode.add(new SourceNode(j+1, 0, files[i], lines[j] + '\n'));
+		}
+		node.add(fileNode);
+
+		node.add(new SourceNode(null, null, null, '\n\n'));
+	}
+
+	node.add(new SourceNode(null, null, null, '}(window, document));'));
+
+	var bundle = node.toStringWithSourceMap();
+	return {
+		src: bundle.code,
+		srcmap: bundle.map.toString()
+	};
+}
+
 function combineFiles(files) {
 	var content = '';
 	for (var i = 0, len = files.length; i < len; i++) {
@@ -98,15 +139,18 @@ exports.build = function (callback, version, compsBase32, buildName) {
 
 	var files = getFiles(compsBase32);
 
-	console.log('Concatenating and compressing ' + files.length + ' files...');
+	console.log('Bundling and compressing ' + files.length + ' files...');
 
 	var copy = fs.readFileSync('src/copyright.js', 'utf8').replace('{VERSION}', version),
-	    intro = '(function (window, document, undefined) {',
-	    outro = '}(window, document));',
-	    newSrc = copy + intro + combineFiles(files) + outro,
-
-	    pathPart = 'dist/leaflet' + (buildName ? '-' + buildName : ''),
+	    filenamePart = 'leaflet' + (buildName ? '-' + buildName : ''),
+	    pathPart = 'dist/' + filenamePart,
 	    srcPath = pathPart + '-src.js',
+	    mapPath = pathPart + '-src.map',
+	    srcFilename = filenamePart + '-src.js',
+	    mapFilename = filenamePart + '-src.map',
+
+	    bundle = bundleFiles(files, copy),
+	    newSrc = bundle.src + '\n//# sourceMappingURL=' + mapFilename,
 
 	    oldSrc = loadSilently(srcPath),
 	    srcDelta = getSizeDelta(newSrc, oldSrc, true);
@@ -115,6 +159,7 @@ exports.build = function (callback, version, compsBase32, buildName) {
 
 	if (newSrc !== oldSrc) {
 		fs.writeFileSync(srcPath, newSrc);
+		fs.writeFileSync(mapPath, bundle.srcmap);
 		console.log('\tSaved to ' + srcPath);
 	}
 
